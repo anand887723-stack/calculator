71e040104b3f5faa411531299ac0dbb489b49f26
anand887723-stack <Anand.Prakash@iiitb.org>
2023-10-30 15:21:56 +0530

    without in java main  logger

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -2,10 +2,10 @@ package calculator;
 
 // package calculator;
 import java.util.*; 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
 public class Main{
-	private static final Logger logger = LogManager.getLogger(Main.class);
+	// private static final Logger logger = LogManager.getLogger(Main.class);
 	public static void main(String[] args){
 		Scanner reader = new Scanner(System.in);
 		int op, flag=0;
@@ -76,8 +76,8 @@ public class Main{
 		System.out.println("The Result is "+c);
 		System.out.println("");
 
-		logger.info("[SQUARE_ROOT OPERATION] [SUCCESS] " + num);
-        logger.info("[SQUARE_ROOT OPERATION] [RESULT] " + c);
+		// logger.info("[SQUARE_ROOT OPERATION] [SUCCESS] " + num);
+        // logger.info("[SQUARE_ROOT OPERATION] [RESULT] " + c);
 
 		return c;
 	}
@@ -89,8 +89,8 @@ public class Main{
 		System.out.println("The Result is "+c);
 		System.out.println("");
 		
-		logger.info("[FACTORIAL OPERATION] [SUCCESS] " + num);
-        logger.info("[FACTORIAL OPERATION] [RESULT] " + c);
+		// logger.info("[FACTORIAL OPERATION] [SUCCESS] " + num);
+        // logger.info("[FACTORIAL OPERATION] [RESULT] " + c);
 
 		return c;
 	}
@@ -100,8 +100,8 @@ public class Main{
 		System.out.println("The Result is "+ c);
 		System.out.println("");
 
-		logger.info("[LOGARITHM OPERATION] [SUCCESS] " + num);
-        logger.info("[LOGARITHM OPERATION] [RESULT] " + c);
+		// logger.info("[LOGARITHM OPERATION] [SUCCESS] " + num);
+        // logger.info("[LOGARITHM OPERATION] [RESULT] " + c);
 		
 		return c;
 	}
@@ -111,8 +111,8 @@ public class Main{
 		System.out.println("The Result is "+ c);
 		System.out.println("");
 		
-		logger.info("[POWER OPERATION] [SUCCESS] " + num);
-        logger.info("[POWER OPERATION] [RESULT] " + c);
+		// logger.info("[POWER OPERATION] [SUCCESS] " + num);
+        // logger.info("[POWER OPERATION] [RESULT] " + c);
 
 		return c;
 	}
